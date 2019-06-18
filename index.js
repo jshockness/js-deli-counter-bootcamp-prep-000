bf6595f42710143046ed9f71831524ada3af4c43
Jason Shockness <jshock12@gmail.com>
2019-06-18 11:41:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine(line) {
   for(i = 0, l = line.length; i < l; i++) {
     numberAndNames.push(`${i + 1}. ${line[i]}`)
     
-    return `The line is currently: ${numberAndNames
+    return `The line is currently: ${numberAndNames.join
   }
   
   if (line.length) {
