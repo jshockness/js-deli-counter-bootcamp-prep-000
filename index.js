c1f8d1e8a20a95b4c92c3c6e41b6c744d2df6c6a
Jason Shockness <jshock12@gmail.com>
2019-06-18 11:38:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(line) {
   var numberAndNames = []
   
   for(i = 0, l = line.length; i < l; i++) {
-    numberAndNames.push(`${i + 1)
+    numberAndNames.push(`${i + 1})
   }
   
   if (line.length) {
