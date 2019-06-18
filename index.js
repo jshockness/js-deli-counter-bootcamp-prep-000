abeaa86306c1e42349ad43e09791785662c461f5
Jason Shockness <jshock12@gmail.com>
2019-06-18 12:09:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(line, name) {
 }
 
 function nowServing(line) {
-  if (!line.length === 0) {
+  if (line.length === 0) {
     return 'There nobody waiting to be served'
   }
   
@@ -13,7 +13,7 @@ function nowServing(line) {
 }
 
 function currentLine(line) {
-  if (!line.length === 0) {
+  if (line.length === 0) {
     return "The line is currently empty."
   }
   
