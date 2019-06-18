e8b943f2c1196c060b851efceef1d7c4a7af7f25
Jason Shockness <jshock12@gmail.com>
2019-06-18 12:33:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,11 +17,12 @@ function currentLine(line) {
     return "The line is currently empty."
   }
   else{
-  var numberAndNames = []
+    var numberAndNames = []
   
   for(let i = 0, l = line.length; i < l; i++) {
     numberAndNames.push(`${i + 1}. ${line[i]}`)
     
     return `The line is currently: ${numberAndNames}`
+    }
   }
 }
