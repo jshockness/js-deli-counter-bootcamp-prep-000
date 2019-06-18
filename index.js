2adb5085a659fa93203bf37ce3edb0503d466aa0
Jason Shockness <jshock12@gmail.com>
2019-06-18 12:34:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,13 +16,12 @@ function currentLine(line) {
   if (line.length === 0) {
     return "The line is currently empty."
   }
-  else{
+  
     var numberAndNames = []
   
   for(let i = 0, l = line.length; i < l; i++) {
     numberAndNames.push(`${i + 1}. ${line[i]}`)
     
     return `The line is currently: ${numberAndNames}`
-    }
   }
 }
