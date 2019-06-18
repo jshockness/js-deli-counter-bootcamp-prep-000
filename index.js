63128a92049ca60c835c6ca984cbcbcbb687a190
Jason Shockness <jshock12@gmail.com>
2019-06-18 11:34:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function nowServing(line) {
 function currentLine(line) {
   var numberAndNames = []
   
-  for(i = 0, l = line
+  for(i = 0, l = line.length
   
   if (line.length) {
     return "The line is currently empty."
